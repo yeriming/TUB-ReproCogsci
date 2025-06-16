4c8cee6b46e46f8d88bbcec8fca33c43eaeaf0fb
Sein <seinjeung@gmail.com>
2025-06-16 16:37:02 +0200

    Compute stride times for the right foot as well

--- gaits_02_extract_features.py
+++ gaits_02_extract_features.py
@@ -57,7 +57,7 @@ max_n_participant = 2  # maximal number of participants per group
 
 # Analysis parameters
 rise_fall_edge = 500
-local_minima_window = 5  # samples
+local_minima_window = 33  # samples
 offset_contact = 10  # in Newton
 
 for group in participant_groups:
@@ -91,23 +91,39 @@ for group in participant_groups:
 
                 # step 2. find local minima near each edge
                 # ------------------------------------------------------------------------------------------------------
-                slope_minima = []
+                # left foot
+                slope_minima_left = []
                 for idx in edgesLeft:
                     # Example: assume you want to search FORWARD in time only
                     min_idx = find_slope_min(left_force, idx, direction=1, window=5, slope_thresh=0.01)
-                    slope_minima.append(min_idx)
+                    slope_minima_left.append(min_idx)
 
-                slope_minima = np.array(slope_minima)
+                slope_minima_left = np.array(slope_minima_left)
 
                 left_force_deriv = np.diff(left_force)
                 time_deriv = time[:-1]  # diff shortens length by 1
 
+                # right foot
+                slope_minima_right = []
+                for idx in edgesLeft:
+                    # Example: assume you want to search FORWARD in time only
+                    min_idx = find_slope_min(right_force, idx, direction=1, window=5, slope_thresh=0.01)
+                    slope_minima_right.append(min_idx)
+
+                slope_minima_right = np.array(slope_minima_right)
+
+                left_force_deriv = np.diff(right_force)
+                time_deriv = time[:-1]  # diff shortens length by 1
+
                 # step 3. For each minimum, find FC
                 #-------------------------------------------------------------------------------------------------------
-                FC_times = []
-                FC_indices = []
+                FC_times_left = []
+                FC_indices_left = []
+                FC_times_right = []
+                FC_indices_right = []
 
-                for min_idx in slope_minima:
+                # left foot
+                for min_idx in slope_minima_left:
                     threshold = left_force[min_idx] - offset_contact
 
                     # Search forward for FC
@@ -115,22 +131,40 @@ for group in participant_groups:
                     while FC_idx < len(left_force) - 1 and left_force[FC_idx] < threshold:
                         FC_idx += 1
 
-                    FC_times.append(time[FC_idx])
-                    FC_indices.append(FC_idx)
+                    FC_times_left.append(time[FC_idx])
+                    FC_indices_left.append(FC_idx)
+
+                # right foot
+                for min_idx in slope_minima_right:
+                    threshold = left_force[min_idx] - offset_contact
+
+                    # Search forward for FC
+                    FC_idx = min_idx
+                    while FC_idx < len(right_force) - 1 and right_force[FC_idx] < threshold:
+                        FC_idx += 1
+
+                    FC_times_right.append(time[FC_idx])
+                    FC_indices_right.append(FC_idx)
 
                 # step 4. compute stride times
-                stride_times = []
-                for step_idx in range(1, len(FC_indices)-1):
-                    stride_time = FC_times[step_idx + 1] - FC_times[step_idx]
-                    stride_times.append(stride_time)
+                stride_times_left = []
+                stride_times_right = []
+                for step_idx in range(1, len(FC_indices_left)-1):
+                    stride_time = FC_times_left[step_idx + 1] - FC_times_left[step_idx]
+                    stride_times_left.append(stride_time)
 
-                out_data = np.column_stack((stride_time, stride_time))
+                for step_idx in range(1, len(FC_indices_right)-1):
+                    stride_time = FC_times_right[step_idx + 1] - FC_times_right[step_idx]
+                    stride_times_right.append(stride_time)
+
+
+                out_data = np.column_stack((stride_times_left, stride_times_right))
 
                 # resave the resulting file in "raw-data folder" following BIDS conventions
                 out_dir = results_folder + '/'
                 os.makedirs(out_dir, exist_ok=True)  # when saving anything, the target folder needs to exist first
                 out_file = 'sub-' + participant_ID + '_strides.tsv'
-                np.savetxt(out_dir + out_file, stride_times, delimiter='\t', fmt='%.6f')
+                np.savetxt(out_dir + out_file, out_data, delimiter='\t', fmt='%.6f')
 
                 # visualize step 1. rising and falling edges------------------------------------------------------------
                 plt.plot(time[1:2000], left_force[1:2000], label='Left Foot Total Force')
@@ -142,37 +176,17 @@ for group in participant_groups:
                 plt.show()
                 # ------------------------------------------------------------------------------------------------------
 
-
-                # # visualize step 2. derivative and vertical lines
-                # plt.plot(time_deriv[1:2000], left_force_deriv[1:2000], label='Left Force Derivative')
-                # for i, idx in enumerate(edgesLeft[edgesLeft < 2000]):
-                #     # Adjust index for derivative (because np.diff shifts by -1)
-                #     plt.axvline(time_deriv[idx], color='m', ls='--', label='Edge' if i == 0 else None)
-                #
-                # # slope_minima
-                # valid_minima = slope_minima[slope_minima < len(time_deriv)]
-                # plt.plot(time_deriv[valid_minima[valid_minima < 2000]],
-                #          left_force_deriv[valid_minima[valid_minima < 2000]],
-                #          'ko', label='Slope-based Min')
-                #
-                # plt.xlabel('Time [s]')
-                # plt.ylabel('Derivative of Force [N/s]')
-                # plt.legend()
-                # plt.show()
-                # # ------------------------------------------------------------------------------------------------------
-
-
                 # Plot only the first 2000 data points
                 plt.plot(time[:2000], left_force[:2000], label='Force signal')
 
                 # show thresholds only for minima in first 2000 points
-                for min_idx in slope_minima:
+                for min_idx in slope_minima_left:
                     if min_idx < 2000:
                         threshold = left_force[min_idx] - offset_contact
                         plt.axhline(threshold, color='grey', linestyle='--', alpha=0.5)
 
                 # Mark FC points only if they are within first 2000
-                FC_in_range = [i for i in FC_indices if i < 2000]
+                FC_in_range = [i for i in FC_indices_left if i < 2000]
                 plt.plot([time[i] for i in FC_in_range], [left_force[i] for i in FC_in_range],
                         'ro', label='Final Contacts')
 
