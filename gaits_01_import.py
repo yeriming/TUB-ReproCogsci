323cd944f59fb4dd41ebb201ecdc39d8e0407436
Sein <seinjeung@gmail.com>
2025-06-16 13:41:18 +0200

    Import data from all participants

--- gaits_01_import.py
+++ gaits_01_import.py
@@ -1,21 +1,54 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Load the data
-data = np.loadtxt('C:/Users/seinj/OneDrive/Desktop/ReproCogsciSoSe25/Data/00_source-data/GaCo01_01.txt')  # Or use pandas if working with CSV
+# Definitions
+data_folder = "P:/Sein_Jeung/Teaching/ReproResearch/Data"
+#figure_folder = "P:/Sein_Jeung/Teaching/ReproResearch/Figures"
+subset_name = "Ga"  # enter one of the following : Ju, Si, Ga
+participant_groups = "Co", "Pt"  # Co for controls, Pt for patients
+max_n_participant = 33  # maximal number of participants per group
 
-# parse data into variables
-time = data[:, 0]
-left_sensors = data[:, 1:8]
-right_sensors = data[:, 9:16]
-left_force = data[:, 17]
-right_force = data[:, 18]
+for group in participant_groups:
+    for pi in range(1, max_n_participant + 1):  # goes up to 33, the max nr of participants in a group
+        for rep in ('01', '02', '10'):
+            try:
+                # participant string
+                participant_ID = subset_name + group + f"{pi:02}"
 
-# Visualise total force data
-plt.plot(time[1:2000], left_force[1:2000], label='Left Foot Total Force')
-#plt.plot(time, right_force, label='Right Foot Total Force')
-#plt.ylabel('Force (N)')
-#plt.legend()
-#plt.title('Total Vertical Ground Reaction Force')
-plt.show()
+                # Load the data
+                data = np.loadtxt(data_folder + '/00_source-data/' + participant_ID + '_' + rep + '.txt')
+
+                # parse data into variables
+                time = data[:, 0]
+                left_force = data[:, 17]
+                right_force = data[:, 18]
+
+                # select only the relevant data (for simplicity)
+                out_data = np.column_stack((time, left_force, right_force))
+
+                # resave the resulting file in "raw-data folder" following BIDS conventions
+                out_dir = data_folder + '/01_raw-data/sub-' + participant_ID + '/beh/'
+                os.makedirs(out_dir, exist_ok=True)  # when saving anything, the target folder needs to exist first
+                out_file = 'sub-' + participant_ID + '_run-' + rep + '_task-gait_beh.tsv'
+                np.savetxt(out_dir + out_file, out_data, delimiter='\t', fmt='%.6f')
+
+                # # Visualise total force data
+                # plt.plot(time[1:2000], left_force[1:2000], label='Left Foot Total Force')
+                # plt.plot(time[1:2000], right_force[1:2000], label='Right Foot Total Force')
+                # plt.ylabel('Force (N)')
+                # plt.legend()
+                # plt.title('Total Vertical Ground Reaction Force')
+                # # plt.show()
+                #
+                # # save the figure
+                # figure_filename = figure_folder + '/01_raw-data/' + 'raw_' + participant_ID + '_' + rep + '.png'
+                # os.makedirs(figure_folder + '/01_raw-data/', exist_ok=True)  # when saving anything, the target folder needs to exist first
+                # plt.savefig(figure_filename)
+                # plt.close()
+
+            except:  #these lines are executed when some files do not exist
+                pass #    print('Could not import ' + participant_ID + '_' + rep + '.txt')
+            else:
+                print('Imported ' + participant_ID + '_' + rep + '.txt')
 
