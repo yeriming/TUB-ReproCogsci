8f787c89db7b3119b1dbfaf9ad41d68c439d5180
Sein <seinjeung@gmail.com>
2025-07-14 15:11:22 +0200

    Replace 0 values with nans

--- gait_03_summary.py
+++ gait_03_summary.py
@@ -18,9 +18,11 @@ group_stride_averages = {
 for group in participant_groups:
     for pi in range(1, max_n_participant + 1):  # goes up to 33, the max nr of participants in a group
 
+        # Initialize participant stride list
+        participant_stride_times = []
+
         # participant string
         participant_ID = subset_name + group + f"{pi:02}"
-        participant_stride_times = []
 
         # construct file path
         file_path = os.path.join(
@@ -36,12 +38,12 @@ for group in participant_groups:
 
         participant_stride_times.append(data)
 
-        if participant_stride_times:
-            # Stack stride times and compute mean across all reps
-            all_strides = np.vstack(participant_stride_times)
-            left_mean = np.nanmean(all_strides[:, 0])
-            right_mean = np.nanmean(all_strides[:, 1])
-            group_stride_averages[group].append([left_mean, right_mean])
+        # Stack stride times and compute mean across all reps
+        all_strides = np.vstack(participant_stride_times)
+        all_strides[all_strides == 0] = np.nan # because there are some zero values mixed in
+        left_mean = np.nanmean(all_strides[:, 0])
+        right_mean = np.nanmean(all_strides[:, 1])
+        group_stride_averages[group].append([left_mean, right_mean])
 
 # Convert lists to arrays
 group_stride_averages["Co"] = np.array(group_stride_averages["Co"])
